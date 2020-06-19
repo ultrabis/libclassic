ed46a26b8c7ad2877e798906a486fb70d30f0980
kmmiles <kenneth.mmiles@gmail.com>
2020-06-18 17:51:06 -0700

    use ts-node for cli execution

--- cli/playground.ts
+++ cli/playground.ts
@@ -1,11 +1,22 @@
+#!/usr/bin/env node
+
 import classic from '../dist'
-import MagicSchool from '../dist/enum/MagicSchool'
+// import classic from '../src'
 
+let myOptions = classic.DefaultOptions.defaults
+
+// let myEncounter = new classic.Encounter(myOptions)
+
+let myEquipment = classic.Optimal.equipment(myOptions)
+
+console.log(myEquipment)
+
+/*
 let mySpell = new classic.Spell('Starfire Rank 1')
-
 let myText = classic.Common.magicSchoolToText(classic.MagicSchool.Holy)
 let myMagicSchool = classic.Common.magicSchoolFromText('Holy')
 
 console.log(`hello world: ${mySpell.avgDmg}`)
 console.log(`hello magic school: ${myText}`)
 console.log(`Hello my magic school: ${myMagicSchool}`)
+*/
